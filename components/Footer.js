c9428dd9906b75f3484985144c89f437ba423b7b
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-08-21 17:34:55 +0000

    cookie

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer({CookieStands}) {
     return (
       <footer className="p-4 text-center text-white bg-green-500">
-        <p>{CookieStands.length} Locations Worldwide</p>
+        <p>{CookieStands.length} Locations World Wide</p>
         &copy; Mohammad Al-shiyab @2023
       </footer>
     );
